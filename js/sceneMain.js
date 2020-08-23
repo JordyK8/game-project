6cd14943ea2744205a3821fc0be99b016cec96bc
Joca Koyi <jocakoyi@Jocas-MacBook-Pro.local>
2020-08-23 11:10:45 +0200

    walking char

---
+++ js/sceneMain.js
@@ -0,0 +1,33 @@
+class SceneMain extends Phaser.Scene{
+    constructor(){
+        super('SceneMain');
+    }
+    preload(){
+        //load our images or sounds
+
+        this.load.spritesheet('boy', 'images/boy.png', { frameWidth: 120, frameHeight: 200 });
+    }
+    create(){
+        //define our objects
+
+        this.char = this.add.sprite(game.config.width/2,game.config.height/2, 'boy');
+       
+        const frameNames = this.anims.generateFrameNumbers('boy');
+
+        this.anims.create({
+            key: 'walk',
+            frames: frameNames,
+            frameRate: 12,
+            repeat: -1
+        });
+        this.char.play('walk');
+        
+    }
+    update(){
+        //constant running loop
+        this.char.x+=2;
+        if(this.char.x>game.config.width){
+            this.char.x=0;
+        }
+    }
+}
